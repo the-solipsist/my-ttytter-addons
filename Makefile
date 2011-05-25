130aa01fb65ecba1d62f6060a988dd41fd27197f
Peter Reuterås <peter@reuteras.com>
2011-05-26 00:11:59 +0200

    Add -k to test because of cert problem with Fedora and Yahoo

--- Makefile
+++ Makefile
@@ -9,9 +9,9 @@ test-limit:
 	echo "/limit" | ./bin/beta -exts=rate_limit_oauth.pl -ssl -simplestart | egrep "Remaining hits [0-9]{3}"
 test-later:
 	echo "/later c9 test hudson-test" | ./bin/ttytter -exts=later.pl -ssl -simplestart | grep "Added to delicious.com"
-	curl -s -u `cat ~/.delicious` https://api.del.icio.us/v1/posts/delete?url=`cat hudson-ttytter-later-test`
+	curl -s -k -u `cat ~/.delicious` https://api.del.icio.us/v1/posts/delete?url=`cat hudson-ttytter-later-test`
 	rm -f hudson-ttytter-later-test
 	echo "/later c9 test hudson-test" | ./bin/beta -exts=later.pl -ssl -simplestart | grep "Added to delicious.com"
-	curl -s -u `cat ~/.delicious` https://api.del.icio.us/v1/posts/delete?url=`cat hudson-ttytter-later-test`
+	curl -s -k -u `cat ~/.delicious` https://api.del.icio.us/v1/posts/delete?url=`cat hudson-ttytter-later-test`
 	rm -f hudson-ttytter-later-test
 	
--- later.pl
+++ later.pl
@@ -10,6 +10,7 @@ $addaction = sub {
 		my $desc=$2;
 		my $testing=$4;
 		my $tweet=&get_tweet($tweet_id);
+        my $content;
 		if (!$tweet->{'id_str'}) {
 		    print $stdout "-- sorry, no such tweet (yet?).\n";
 		    return 1;
@@ -18,15 +19,20 @@ $addaction = sub {
 		my $add_url="http://twitter.com/$tweet->{'user'}->{'screen_name'}/statuses/$tweet->{'id'}";
 		my $delicious_url="https://api.del.icio.us/v1/posts/add?url=" .
 			$add_url . "&tags=\@ttytter_added\@&shared=no&description=$desc";
-		my $content = `curl -s -u \"$del_pass\" \"$delicious_url\"`;
+		if ($testing == "hudson-test"){
+		    $content = `curl -k -s -u \"$del_pass\" \"$delicious_url\"`;
+		}else{ 
+		    $content = `curl -s -u \"$del_pass\" \"$delicious_url\"`;
+        }
 		if ($content =~ m!<result code="done" />!){
 			print "Added to delicious.com\n";
 			if ($testing == "hudson-test"){
 				`echo "$add_url" > hudson-ttytter-later-test`;
 			}
 		}else{ 
-			print "Something went wrong, not added. Se response:\n";
+			print "Something went wrong, not added. See response:\n" if $content;
 			print $content;
+            print "Something went wrong, probably because of an SSL problem.\n" if not $content;
 		}
 		return 1;
 	}
