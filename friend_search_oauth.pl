bfe24348e8751388dedf8698e6b684ca343f9419
Peter Reuterås <peter@reuteras.com>
2012-12-01 19:10:05 +0100

    Change to GNU GPLv3

--- friend_search_oauth.pl
+++ friend_search_oauth.pl
@@ -1,7 +1,21 @@
 # Command to search among the users friends
 #
-# Every 100 will decreate your rate limit with one
+# Copyright (C) 2012 Peter Reuterås
 #
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Every 100  friends will decreate your rate limit with one
 
 $addaction = sub {
 	my $command = shift;
--- rate_limit_oauth.pl
+++ rate_limit_oauth.pl
@@ -1,10 +1,19 @@
 # Command to check the current rate limit status
 #
-# Copyright 2009 Peter Reuterås
+# Copyright (C) 2012 Peter Reuterås
 #
-# License: GPL v2
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.                                                                                                                            
 #
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 $addaction = sub {
 	my $command = shift;
