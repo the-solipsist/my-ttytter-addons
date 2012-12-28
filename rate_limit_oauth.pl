e626ed955db4daf2424e39b23ed0eb9e2c24f722
Peter Reuterås <peter@reuteras.com>
2012-12-28 19:31:03 +0100

    Update rate limit code and test.

--- rate_limit_oauth.pl
+++ rate_limit_oauth.pl
@@ -21,13 +21,15 @@ $addaction = sub {
 	if ($command =~ s#^/limit##) {
 		my $result = &grabjson("$rlurl");
 		if (defined($result) && ref($result) eq 'HASH') {
-			my $reset_time_in_seconds=$result->{'reset_time_in_seconds'};
-			(my $sec,my $min,my $hour,my $mday,my $mon,my $year,my $wday,my $yday,
-				my $isdst)=localtime($reset_time_in_seconds);
-			my $reset_time = sprintf "%4d-%02d-%02d %02d:%02d:%02d", 
-				$year+1900,$mon+1,$mday,$hour,$min,$sec;
-			print "Remaining hits ".$result->{'remaining_hits'}.", resets at ".
-				$reset_time.". Hourly limit is ".$result->{'hourly_limit'}.".\n";
+		    print "Limit period is 15 min, see https://dev.twitter.com/docs/api/1.1/get/application/rate_limit_status for more info.\n";
+		    foreach $status ('/statuses/user_timeline', '/statuses/mentions_timeline', '/statuses/home_timeline', '/statuses/show/:id', '/statuses/retweets/:id'){
+			    my $reset_time_in_seconds=$result->{'resources'}{'statuses'}{$status}{reset};
+			    my $reset_limit=$result->{'resources'}{'statuses'}{$status}{limit};
+			    my $reset_remaining=$result->{'resources'}{'statuses'}{$status}{remaining};
+			    (my $sec, my $min, my $hour, my $mday, my $mon, my $year, my $wday, my $yday, my $isdst)=localtime($reset_time_in_seconds);
+    			my $reset_time = sprintf "%4d-%02d-%02d %02d:%02d:%02d", $year+1900,$mon+1, $mday, $hour, $min, $sec;
+			    print "Remaining hits for ".$status. " is ".$reset_remaining. "/".$reset_limit.", resets at ". $reset_time.".\n";
+    		}
 			return 1;
       }
 	}
