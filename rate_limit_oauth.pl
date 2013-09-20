850f253bcff7e6e241d0b914eb070a7d0fdcf55f
Peter Reuterås <peter@reuteras.com>
2013-09-20 21:49:44 +0200

    Use Data::Dumper to print. To many rate limits to iterate.

--- rate_limit_oauth.pl
+++ rate_limit_oauth.pl
@@ -20,18 +20,8 @@ $addaction = sub {
 
 	if ($command =~ s#^/limit##) {
 		my $result = &grabjson("$rlurl");
-		if (defined($result) && ref($result) eq 'HASH') {
-		    print "Limit period is 15 min, see https://dev.twitter.com/docs/api/1.1/get/application/rate_limit_status for more info.\n";
-		    foreach $status ('/statuses/user_timeline', '/statuses/mentions_timeline', '/statuses/home_timeline', '/statuses/show/:id', '/statuses/retweets/:id'){
-			    my $reset_time_in_seconds=$result->{'resources'}{'statuses'}{$status}{reset};
-			    my $reset_limit=$result->{'resources'}{'statuses'}{$status}{limit};
-			    my $reset_remaining=$result->{'resources'}{'statuses'}{$status}{remaining};
-			    (my $sec, my $min, my $hour, my $mday, my $mon, my $year, my $wday, my $yday, my $isdst)=localtime($reset_time_in_seconds);
-    			my $reset_time = sprintf "%4d-%02d-%02d %02d:%02d:%02d", $year+1900,$mon+1, $mday, $hour, $min, $sec;
-			    print "Remaining hits for ".$status. " is ".$reset_remaining. "/".$reset_limit.", resets at ". $reset_time.".\n";
-    		}
-			return 1;
-      }
+		use Data::Dumper or die("This command needs Data::Dumper to work.");
+		print Dumper(\$result);
 	}
 	return 0;
 };
